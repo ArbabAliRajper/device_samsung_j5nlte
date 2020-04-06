62a1bbfd54cef372babc5ffeaa811bf6f6e5fb36
Resurrection <30309647+Resurrection3387@users.noreply.github.com>
2020-04-06 15:55:15 +0300

    j5nlte: add PRODUCT_BRAND

--- aosp.mk
+++ aosp.mk
@@ -15,7 +15,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
