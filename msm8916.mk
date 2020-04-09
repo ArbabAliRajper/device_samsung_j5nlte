0dda0d02f95fd38d39235a6b194e3bb25b20868a
Resurrection <30309647+Resurrection3387@users.noreply.github.com>
2020-04-09 09:28:39 +0300

    j5nlte: completely get rid of SamsungParts

--- msm8916.mk
+++ msm8916.mk
@@ -282,10 +282,6 @@ PRODUCT_PACKAGES += \
     libkeyutils \
     tcpdump
 
-# Parts
-PRODUCT_PACKAGES += \
-    SamsungParts
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
