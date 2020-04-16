c8dbc2524130959fb39c33c7db07b290d51c3d6a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-04-16 09:34:34 +0200

    j5nlte: fix device-specific overlays

--- device-j5x.mk
+++ device-j5x.mk
@@ -2,6 +2,6 @@
 $(call inherit-product, vendor/samsung/j5x-common/j5x-common-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay_j5x
+    $(LOCAL_PATH)/overlay-j5x
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth_j5x
