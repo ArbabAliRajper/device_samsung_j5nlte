1cc00a49690c8751b6240d5e96b73a201d74cb69
Resurrection <30309647+Resurrection3387@users.noreply.github.com>
2020-04-17 11:45:28 +0300

    j5nlte: use - instead of _ for bluetooth Make it similar to overlay-j5 and overlay-j5x

--- device-j5.mk
+++ device-j5.mk
@@ -4,4 +4,4 @@ $(call inherit-product, vendor/samsung/j5-common/j5-common-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-j5
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth_j5
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth-j5
