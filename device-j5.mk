2cf28ed2e4333fcaa96ae72fc0597492a38ca0ae
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-04-21 09:13:55 +0200

    j5nlte: remove battery capacity overlay * Android is able to pick this value from kernel

--- device-j5.mk
+++ device-j5.mk
@@ -16,7 +16,4 @@
 # Inherit from vendor
 $(call inherit-product, vendor/samsung/j5-common/j5-common-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-j5
-
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth-j5
--- device-j5x.mk
+++ device-j5x.mk
@@ -16,7 +16,4 @@
 # Inherit from vendor
 $(call inherit-product, vendor/samsung/j5x-common/j5x-common-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-j5x
-
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth-j5x
