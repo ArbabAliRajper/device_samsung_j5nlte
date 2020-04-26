d461623bde429fab602d7757dcb65fe4a6919ed4
Resurrection3387 <arnislin@gmail.com>
2020-04-26 11:19:25 +0300

    j5nlte: update copyright years

--- gps/core/gps_extended_c.h
+++ gps/core/gps_extended_c.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, 2016 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2020, 2016 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
