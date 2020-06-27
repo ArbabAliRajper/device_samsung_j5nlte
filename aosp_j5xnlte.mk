c5404c6d92eb83975c6a501c5e22dfb6aa801039
Arbab Ali Rajpar <arbabaliabforever@gmail.com>
2020-06-27 18:38:05 +0500

    Pixel bringup

--- aosp_j5xnlte.mk
+++ aosp_j5xnlte.mk
@@ -31,20 +31,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device.mk
-$(call inherit-product, device/samsung/j5nlte/device.mk)
+$(call inherit-product, device/samsung/j5xnlte/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-PRODUCT_DEVICE := j5nlte
-PRODUCT_NAME := aosp_j5nlte
-PRODUCT_MODEL := SM-J500FN
+PRODUCT_DEVICE := j5xnlte
+PRODUCT_NAME := aosp_j5xnlte
+PRODUCT_MODEL := SM-J510FN
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="j5nltexx-user 10 MMB29M J500FXXU1BSK2 release-keys"
+    PRIVATE_BUILD_DESC="j5xnltexx-user 10 MMB29M J510FXXU1BSK2 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=google/flame/flame:10/QQ2A.200405.005/6254899:user/release-keys
